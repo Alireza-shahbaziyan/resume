92f6a40c7293dfbe0f4f9f9f54b9fa68d690361d
alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-05-26 20:15:01 +0430

    update

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var nameItem = '';
 var count = 0;
 var nameId = document.getElementById('NameTitle');
 
- function write() {
+function write() {
     nameItem += Name[count];
     nameId.innerHTML = nameItem;
     count++;
-    if (count >= Name.length) {
-        clearInterval(myIntervalName);
-    }
+    count >= Name.length && clearInterval(myIntervalName);
+
+
 }
 
-var myIntervalName =  setInterval(write, 100);
+var myIntervalName = setInterval(write, 100);
 // ----------write Age
 var myDate = new Date(2001, 07, 19);
 var nowDate = new Date();
-document.getElementById('age').innerHTML= +nowDate.getFullYear() - myDate.getFullYear();
+document.getElementById('age').innerHTML = +nowDate.getFullYear() - myDate.getFullYear();
